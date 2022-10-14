7aa836dded0b058def2736b7009eda9dd9115fce
Louizz7 <chibuzorlouizz@gmail.com>
2022-10-14 13:49:30 -0700

    8. Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -14,11 +14,12 @@ int main(void)
 	int i;
 	char low;
 
-	for (low = 'a'; low <= 'f'; low++)
-	putchar(low);
 
 	for (i = '0'; i <= '9'; i++)
 	putchar(i);
+
+	for (low 'a'; low <= 'f'; low++)
+	putchar(low);
 	putchar('\n');
 
 	return (0);
