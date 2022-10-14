4574f9dbb4cd767327d4d47ccf78883995abe77b
Louizz7 <chibuzorlouizz@gmail.com>
2022-10-14 06:55:06 -0700

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char low;
+
+	for (low 'a'; low <= 'z'; low++)
+		putchar(low);
+
+	char high;
+
+	for (high 'A'; low <= 'Z'; high++)
+		putchar(high);
+		putcher("\n");
+
+	return (0);
+}
