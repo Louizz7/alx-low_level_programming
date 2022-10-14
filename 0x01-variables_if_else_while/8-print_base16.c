3e68800f0790794d67c36b60ad9373d2e00dbb33
Louizz7 <chibuzorlouizz@gmail.com>
2022-10-14 13:42:16 -0700

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	int i;
+	char low;
+
+	for (low = 'a'; low <= 'f'; low++)
+	putchar(low);
+
+	for (i = '0'; i = '9'; i++)
+	putchar(i);
+	putchar('\n');
+
+	return (0);
+}
