eb2054f78d58c530157e4839c8b2fd42f831501e
Louizz7 <chibuzorlouizz@gmail.com>
2022-10-14 16:35:10 -0700

    11. The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,10 +14,13 @@ int main(void)
 
 	for (a = 0; a < 8; a++)
 	{
+	
 		for (b = a + 1; b < 9; b++)
 		{
+		
 			for (c = b +1; c < 10; c++)
 			{
+			
 				putchar((a % 10) + '0');
 				putchar((b % 10) + '0');
 				putchar((c % 10) + '0');
@@ -27,10 +30,12 @@ int main(void)
 
 				putchar(',');
 				putchar(' ');
-			{
+			}
 		}
 	}
+			
 	putchar('\n');
 
-		return (0);
+	return (0);
 }
+
