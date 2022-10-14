44dcbbcbd581cb0ac6411ac41aec59108f430404
Louizz7 <chibuzorlouizz@gmail.com>
2022-10-14 07:05:00 -0700

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,11 +13,11 @@ int main(void)
 	char low;
 
 	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
+	putchar(low);
 
 	for (low = 'A'; low <= 'Z'; low++)
-		putchar(low);
-		putcher("\n");
+	putchar(low);
+	putcher("\n");
 
 	return (0);
 }
