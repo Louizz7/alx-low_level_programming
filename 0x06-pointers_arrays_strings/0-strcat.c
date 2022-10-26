afce9f73ae75fdfef7f9d7ee16b21f3549a1414b
Louizz7 <chibuzorlouizz@gmail.com>
2022-10-26 07:17:55 -0700

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,7 +11,7 @@
  */
 
 
- char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 
 {
 	int i = 0, dest_len = 0;
@@ -19,7 +19,7 @@
 	while (dest[i++])
 		dest_len++;
 
-	for (i = 0; src[i]; != '\0'; i++)
+	for (i = 0; src[i]; i++)
 		dest[dest_len++] = src[i];
 
 	return (dest);
