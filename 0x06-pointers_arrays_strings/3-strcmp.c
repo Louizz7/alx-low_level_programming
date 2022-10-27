20a695b40c62bb754afdd4a1cb6e1c1b47571c5a
Louizz7 <chibuzorlouizz@gmail.com>
2022-10-26 20:28:32 -0700

    3. strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares pointers to two strings.
+ * @s1: first string pointer
+ * @s2: second string pointer
+ *
+ * Return: integer
+ *
+ */
+
+int _strcmp(char *s1, char *s2)
+
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+
+	return (*s1 - *s2);
+
+}
