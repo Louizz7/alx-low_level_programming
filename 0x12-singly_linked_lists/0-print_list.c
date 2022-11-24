d46be34bddb851c48654d4e3597390a6a3cc3f2d
Louizz7 <chibuzorlouizz@gmail.com>
2022-11-24 08:46:35 +0100

    0. Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,9 +7,8 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nelem;
+	int i = 0;
 
-	nelem = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -17,7 +16,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		nelem++;
+		i++;
 	}
-	return (nelem);
+	return (i);
 }
