5b9e26bfc9e7e1dc057e0f0ef490f767ca509a00
Louizz7 <chibuzorlouizz@gmail.com>
2022-11-28 00:54:13 -0800

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx != 0)
 	{
-		for (i = 0; i < idx - 1 && h != NULL; i++)
+		for (i = 0; i < idx - 1 && node2 != NULL; i++)
 		{
 			node2 = node2->next;
 		}
