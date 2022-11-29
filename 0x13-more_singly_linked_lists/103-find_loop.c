832418aef28cd279667d241fee02b48b8667412b
Louizz7 <chibuzorlouizz@gmail.com>
2022-11-29 02:38:18 -0800

    Find the loop

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -2,49 +2,48 @@
 
 /**
  * main - checks the code
- *
  * Return: Always 0.
  */
 int main(void)
 {
-    listint_t *H1;
-    listint_t *H2;
-    listint_t *node;
+	listint_t *H1;
+	listint_t *H2;
+	listint_t *node;
 
-    head2 = NULL;
-    add_nodeint(&H2, 0);
-    add_nodeint(&H2, 1);
-    add_nodeint(&H2, 2);
-    add_nodeint(&H2, 3);
-    add_nodeint(&H2, 4);
-    add_nodeint(&H2, 98);
-    add_nodeint(&H2, 402);
-    add_nodeint(&H2, 1024);
-    print_listint_safe(H2);
-    node = find_listint_loop(H2);
-    if (node != NULL)
-    {
-        printf("Loop starts at [%p] %d\n", (void *)node, node->n);
-    }
-    free_listint_safe(&H2);
-    head = NULL;
-    node = add_nodeint(&H1, 0);
-    add_nodeint(&H1, 1);
-    add_nodeint(&H1, 2);
-    add_nodeint(&H1, 3);
-    add_nodeint(&H1, 4);
-    add_nodeint(&H1, 5);
-    add_nodeint(&H1, 6);
-    node->next = add_nodeint(&H1, 7);
-    add_nodeint(&H1, 98);
-    add_nodeint(&H1, 402);
-    add_nodeint(&H1, 1024);
-    print_listint_safe(H1);
-    node = find_listint_loop(H1);
-    if (node != NULL)
-    {
-        printf("Loop starts at [%p] %d\n", (void *)node, node->n);
-    }
-    free_listint_safe(&H1);
-    return (0);
+	H2 = NULL;
+	add_nodeint(&H2, 0);
+	add_nodeint(&H2, 1);
+	add_nodeint(&H2, 2);
+	add_nodeint(&H2, 3);
+	add_nodeint(&H2, 4);
+	add_nodeint(&H2, 98);
+	add_nodeint(&H2, 402);
+	add_nodeint(&H2, 1024);
+	print_listint_safe(H2);
+	node = find_listint_loop(H2);
+	if (node != NULL)
+	{
+		printf("Loop starts at [%p] %d\n", (void *)node, node->n);
+	}
+	free_listint_safe(&H2);
+	H1 = NULL;
+	node = add_nodeint(&H1, 0);
+	add_nodeint(&H1, 1);
+	add_nodeint(&H1, 2);
+	add_nodeint(&H1, 3);
+	add_nodeint(&H1, 4);
+	add_nodeint(&H1, 5);
+	add_nodeint(&H1, 6);
+	node->next = add_nodeint(&H1, 7);
+	add_nodeint(&H1, 98);
+	add_nodeint(&H1, 402);
+	add_nodeint(&H1, 1024);
+	print_listint_safe(H1);
+	node = find_listint_loop(H1);
+	if (node != NULL)
+	{
+		printf("Loop starts at [%p] %d\n", (void *)node, node->n);
+	}
+	free_listint_safe(&H1);
+	return (0);
 }
