cad405721872265f68334234f3c4af2e756eff1b
Louizz7 <chibuzorlouizz@gmail.com>
2022-12-05 11:59:52 -0800

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,7 +19,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	buffer = malloc(sizeof(char) * letters);
 
-	if (buffer ==  NUll)
+	if (buffer ==  NULL)
 		return (0);
 	o = open(filename, O_RDONLY);
 	r = read(o, buffer, letters);
